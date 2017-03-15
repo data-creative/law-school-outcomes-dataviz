798eb2650002a656e154eefd9d712f8ed7522a74
MJ Rossetti <datacreativellc@gmail.com>
2017-03-15 13:52:53 -0400

    Begin to convert functional code into object-oriented

---
+++ assets/js/employment_summary_report.js
@@ -0,0 +1,64 @@
+'use strict';
+
+class EmploymentSummaryReport {
+  constructor(data) {
+    this.data = data
+  }
+
+  //
+  // CLASS METHODS
+  //
+
+  static employmentStatuses(){
+    return [
+      "Employed - Bar Passage Required",
+      "Employed - J.D. Advantage",
+      "Employed - Professional Position",
+      "Employed - Non-Professional Position",
+      "Employed - Law School/University Funded",
+      "Employed - Undeterminable",
+      "Pursuing Graduate Degree Full Time",
+      "Unemployed - Start Date Deferred",
+      "Unemployed - Not Seeking",
+      "Unemployed - Seeking",
+      "Employment Status Unknown"
+    ]
+  }
+
+  static employedStatuses(){
+    return this.employmentStatuses().filter(function(status){ return status.includes("Employed") })
+  }
+
+  static unemployedStatuses(){
+    return this.employmentStatuses().filter(function(status){ return !status.includes("Employed") })
+  }
+
+  static employmentTypes(){
+    return [
+      {type: "Law Firms (Unknown Size)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (Solo)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (2 - 10)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (11 - 25)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (26 - 50)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (51 - 100)", defaultDisplayGroup: "Law Firm (Other)"},
+      {type: "Law Firms (101 - 250)", defaultDisplayGroup: "Law Firm (Big)"},
+      {type: "Law Firms (251 - 500)", defaultDisplayGroup: "Law Firm (Big)"},
+      {type: "Law Firms (501 +)", defaultDisplayGroup: "Law Firm (Big)"},
+
+      {type: "Business & Industry", defaultDisplayGroup: "Business & Industry"},
+      {type: "Government", defaultDisplayGroup: "Government"},
+      {type: "Pub. Int.", defaultDisplayGroup: "Public Interest"},
+      {type: "Clerkships - Federal", defaultDisplayGroup: "Clerkship"},
+      {type: "Clerkships - State & Local", defaultDisplayGroup: "Clerkship"},
+      {type: "Clerkships - Other", defaultDisplayGroup: "Clerkship"},
+      {type: "Education", defaultDisplayGroup: "Employed (Other)"},
+      {type: "Employer Type Unknown", defaultDisplayGroup: "Employed (Other)"}
+    ]
+  }
+
+  //
+  // INSTANCE METHODS
+  //
+
+
+}
