cf62d6a2d81634506827496d7866da588ed3f8cd
MJ Rossetti <datacreativellc@gmail.com>
2017-03-15 14:11:09 -0400

    Refactor additional functions to be object-oriented

--- assets/js/employment_summary_report.js
+++ assets/js/employment_summary_report.js
@@ -60,5 +60,28 @@ class EmploymentSummaryReport {
   // INSTANCE METHODS
   //
 
+  //get myAttr(){
+  //  return "Fun Times"
+  //}
+
+  // Returns an array of counts representing graduates belonging to any of the selectedStatuses.
+  // @param [Array] selectedStatuses e.g. e.g. ["Pursuing Graduate Degree Full Time", "Unemployed - Start Date Deferred", "Unemployed - Not Seeking", "Unemployed - Seeking", "Employment Status Unknown"]
+  statusCounts(selectedStatuses) {
+    return this.data.employment_outcomes.statuses.filter(function(statusCount){
+      return selectedStatuses.includes(statusCount.status)
+    }).map(function(statusCount){
+      return statusCount.count
+    })
+  }
+
+  // Returns an array of counts representing graduates employed in any of the selectedTypes.
+  // @param [Array] selectedTypes e.g. ["Law Firms (101 - 250)", "Law Firms (251 - 500)", "Law Firms (501 +)"]
+  typeCounts(selectedTypes) {
+    return this.data.employment_outcomes.types.filter(function(typeCount){
+      return selectedTypes.includes(typeCount.type)
+    }).map(function(typeCount){
+      return typeCount.count
+    })
+  }
 
 }
